0cdc48b171badf77a91e2f427fa6700e684e1c5a
Haris <haris005@gmail.com>
2015-01-21 19:50:16 -0500

    Fixed Merge

--- database.rkt
+++ database.rkt
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #| Assignment 1 - Racket Query Language (due February 11, noon)
 
 ***Write the names and CDF accounts for each of your group members below.***
@@ -28,7 +27,7 @@ Haris Shoaib, g3shoaib
   Returns a list of the attributes in 'table', in the order they appear.
 |#
 ;Jasmin
-(define attributes (void))
+(define (attributes table) (first table))
 
 #|
 (tuples table)
@@ -75,127 +74,6 @@ A function that takes:
   (where-helper-1 
   )
 |#
-#|
-A function that takes:
-  - f: a unary function that takes a tuple and returns a boolean value
-  - table: a valid table
-
-  and returns a new table containing only the tuples in 'table'
-  that satisfy 'f'.
-|#;Jasmin
-
-#|
-A function 'replace-attr' that takes:
-  - x 
-  - a list of attributes
-
-  and returns a function 'f' which takes a tuple and does the following:
-    - If 'x' is in the list of attributes, return the corrresponding value 
-      in the tuple.
-    - Otherwise, just ignore the tuple and return 'x'.
-|#
-
-#|
-;Select/From
--table: list of list
--attribute: list
-return a table with only the columns in the list of aatributes
-;Q:how to allow multiple tables
-Note: * must implement
-
-Where
--boolean: "age == 0"
--table: the tuples
-return only the tuples that satisfy the boolean
-use helper function
-
-
-Orderby
--atributes: list of attributes where the values in that column evaluate to a number
--table: of tuples
-
-return a table sorted in non increasing order of the values
-Q: do we have to be albe to order my multiple attributes one after the other
-|#
-
-; Starter for Part 4; feel free to ignore!
-
-; What should this macro do?
-(define-syntax replace
-  (syntax-rules ()
-    ; The recursive step, when given a compound expression
-    [(replace (expr ...) table)
-     ; Change this!
-     (void)]
-    ; The base case, when given just an atom. This is easier!
-    [(replace atom table)
-     ; Change this!
-     (void)]))
-=======
-#| Assignment 1 - Racket Query Language (due February 11, noon)
-
-***Write the names and CDF accounts for each of your group members below.***
-<Name>, <CDF>
-<Name>, <CDF>
-|#
-#lang racket
-
-; Function versions for common syntactic forms.
-; *Use these in your queries instead of the syntactic forms!!!*
-(define (And x y) (and x y))
-(define (Or x y) (or x y))
-(define (If x y z) (if x y z))
-
-; TODO: After you have defined your macro(s), make sure you add each one
-; to the provide statement.
-(provide attributes
-         tuples
-         size)
-
-; Part 0: Semantic aliases
-
-#|
-(attributes table)
-  table: a valid table (i.e., a list of lists as specified by assigment)
-
-  Returns a list of the attributes in 'table', in the order they appear.
-|#
-;Jasmin
-(define (attributes table) (first table))
-
-#|
-(tuples table)
-  table: a valid table
-
-  Returns a list of all tuples in 'table', in the order they appear.
-  Note: it is possible for 'table' to contain no tuples.
-|#
-;Haris
-(define tuples (void))
-
-#|
-(size table)
-  table: a valid table
-
-  Returns the number of tuples in 'table'.
-|#;Haris
-(define size (void))
-
-
-; Part I "WHERE" helpers; you may or may not wish to implement these.
-
-#|
-A function that takes: 
-  - a list of attributes
-  - a string (representing an attribute)
-  - a tuple
-
-  and returns the value of the tuple corresponding to that attribute.
-|#;Haris
-
-
-
-
 #|
 A function that takes:
   - f: a unary function that takes a tuple and returns a boolean value
@@ -257,4 +135,3 @@ Q: do we have to be albe to order my multiple attributes one after the other
     [(replace atom table)
      ; Change this!
      (void)]))
->>>>>>> Jasmin
